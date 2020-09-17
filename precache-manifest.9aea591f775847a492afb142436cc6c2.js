5afc58bd930d13847acdb6dcf2397e8fc2a5d813
Artur <MrArtur0074@gmail.com>
2020-09-17 19:57:06 +0500

    gh-pages

---
+++ precache-manifest.9aea591f775847a492afb142436cc6c2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090d9a0b3f38cd08d93ce406a603141c",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "9e56ea6e38f6219aaf57",
+    "url": "/test-app/static/css/main.b416428d.chunk.css"
+  },
+  {
+    "revision": "1584764f1f03cd8c62f7",
+    "url": "/test-app/static/js/2.584c77c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-app/static/js/2.584c77c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e56ea6e38f6219aaf57",
+    "url": "/test-app/static/js/main.6c04ca54.chunk.js"
+  },
+  {
+    "revision": "010976abd684515c15d4",
+    "url": "/test-app/static/js/runtime-main.107bf183.js"
+  },
+  {
+    "revision": "53075fd96fd848746a3fac99fe1eed83",
+    "url": "/test-app/static/media/css.53075fd9.svg"
+  },
+  {
+    "revision": "0d10f8f10c38b84eada450482ad46fb4",
+    "url": "/test-app/static/media/delete.0d10f8f1.svg"
+  },
+  {
+    "revision": "eea18703d306b9c8ec78595ecf673e27",
+    "url": "/test-app/static/media/edit.eea18703.svg"
+  },
+  {
+    "revision": "1acf12649c0fc70b4441430701fc1587",
+    "url": "/test-app/static/media/git_icon.1acf1264.svg"
+  },
+  {
+    "revision": "0fb1be87d8b8bd634fa1b8279594c8c6",
+    "url": "/test-app/static/media/jquery-icon.0fb1be87.svg"
+  },
+  {
+    "revision": "7081eedd5b6a9d89d7db4abbf1ba3e3d",
+    "url": "/test-app/static/media/js.7081eedd.webp"
+  },
+  {
+    "revision": "e9bf138c2a4c343bb835384415a4e909",
+    "url": "/test-app/static/media/me.e9bf138c.jpg"
+  },
+  {
+    "revision": "7fff4fb4f82f734248d482a557c29320",
+    "url": "/test-app/static/media/myVK.7fff4fb4.png"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/test-app/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "d956cbbb2e59777272370c854a3b6747",
+    "url": "/test-app/static/media/php.d956cbbb.svg"
+  },
+  {
+    "revision": "1e71bac71dbb669f76025968bfe92ba4",
+    "url": "/test-app/static/media/plus.1e71bac7.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test-app/static/media/react.ee7cd8ed.svg"
+  }
+]);
